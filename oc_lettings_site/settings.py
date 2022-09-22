ec1aff10a0f792c746c7613854d9297748094c29
Karim-Dorado <karimdorado@gmail.com>
2022-09-22 17:04:15 +0200

    config environement variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,18 +1,14 @@
 import os
-from decouple import config
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+env= environ.Env()
+# Reading .env files
+environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
