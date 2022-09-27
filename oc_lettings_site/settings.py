a98ac4b16839dec5d09a08f79c9a0bd263a147d9
Karim-Dorado <karimdorado@gmail.com>
2022-09-27 19:18:48 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -4,7 +4,7 @@ import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env= environ.Env()
+env = environ.Env()
 # Reading .env files
 environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
