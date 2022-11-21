41b7644da2a7176142853e0060d8f9d846b555e5
Karim-Dorado <karimdorado@gmail.com>
2022-11-21 22:09:31 +0100

    update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,8 +15,7 @@ DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
                  '0.0.0.0',
-                 'kd-oc-lettings.herokuapp.com',
-                 'karim-dorado-oc-lettings.herokuapp.com']
+                 'kd-oc-lettings.herokuapp.com']
 
 
 # Application definition
